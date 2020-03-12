b551d1921de07fc026da15f6c598f532ade837d1
VD_Styles <vdovhan23@gmail.com>
2020-03-12 23:23:37 +0200

    #32 Add step after and before api side

--- api/app/Http/Controllers/Snippets/StepController.php
+++ api/app/Http/Controllers/Snippets/StepController.php
@@ -8,27 +8,53 @@ use App\Step;
 use App\Transformers\Snippets\SteptTransformer;
 use Illuminate\Http\Request;
 
-class StepController extends Controller
-{
-    public function update(Snippet $snippet, Step $step, Request $request) {
+class StepController extends Controller {
+    /**
+     * @param Snippet $snippet
+     * @param Step $step
+     * @param Request $request
+     */
+    public function update( Snippet $snippet, Step $step, Request $request ) {
 
         //autorize
 
-        $step->update($request->only(['title', 'body']));
+        $step->update( $request->only( [ 'title', 'body' ] ) );
 
     }
 
-    public function store(Snippet $snippet, Request $request) {
+    /**
+     * @param Snippet $snippet
+     * @param Request $request
+     *
+     * @return array
+     */
+    public function store( Snippet $snippet, Request $request ) {
         //autorize
 
+
         $step = $snippet->steps()->create(
-           array_merge( $request->only(['title', 'body']), ['order' => 1])
+            array_merge( $request->only( [ 'title', 'body' ] ), [ 'order' => $this->getOrder($request) ] )
         );
 
         return fractal()
-            ->item($step)
-            ->transformWith(new SteptTransformer())
+            ->item( $step )
+            ->transformWith( new SteptTransformer() )
             ->toArray();
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return mixed
+     */
+    protected function getOrder( Request $request ) {
+
+        $result = Step::where( 'uuid', $request->before )
+                   ->orWhere('uuid', $request->after )
+                   ->first()
+                   ->{($request->before ? 'before' : 'after')."Order"}();
+
+        return $result;
     }
 
 }
--- api/app/Step.php
+++ api/app/Step.php
@@ -5,20 +5,48 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use App\Snippet;
 
-class Step extends Model
-{
+class Step extends Model {
 
-    public function getRouteKeyName (  ) {
+    public function getRouteKeyName() {
         return 'uuid';
     }
 
 
-    protected $fillable = ['order', 'uuid', 'title', 'snippet_id', 'body'];
+    protected $fillable = [ 'order', 'uuid', 'title', 'snippet_id', 'body' ];
 
-    public function snippet( ) {
-        return $this->belongsTo(Snippet::class);
+    public function snippet() {
+        return $this->belongsTo( Snippet::class );
     }
 
+
+    /**
+     * @return float|int
+     */
+    public function afterOrder() {
+
+        $add = self::where( 'order', '>', $this->order )
+                   ->orderBy( 'order', 'asc' )
+                   ->first();
+
+        if ( ! $add ) {
+            return self::orderBy('order', 'decs')->first()->order + 1;
+        }
+
+        return ( $this->order + $add->order ) / 2;
+    }
+
+
+    public function beforeOrder() {
+        $add = self::where( 'order', '<', $this->order )
+                   ->orderBy( 'order', 'desc' )
+                   ->first();
+
+        if ( ! $add ) {
+            return self::orderBy('order', 'asc')->first()->order - 1;
+        }
+
+        return ( $this->order + $add->order ) / 2;
+    }
 
 }
 
